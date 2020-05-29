c52bae64672f856fa90140c0ef1271fb151e7e07
William Lin <wlin26@yahoo.com>
2020-05-28 21:46:12 -0400

    Updated management code to terminate old instance and create new one

--- scripts/module_02/create-ec2-instance.js
+++ scripts/module_02/create-ec2-instance.js
@@ -8,8 +8,8 @@ AWS.config.update({ credentials, region: 'us-east-1' });
 // Declare local variables
 const DryRun = false;
 const ec2 = new AWS.EC2();
-const sgName = 'hamster_sg';
-const keyName = 'hamster_key';
+const sgName = 'hamster_sg2';
+const keyName = 'hamster_key2';
 const tags = [
   {
     Key: 'App',
@@ -105,7 +105,7 @@ function createKeyPair(keyName) {
 
 function createInstance(sgName, keyName) {
   const params = {
-    ImageId: 'ami-0323c3dd2da7fb37d',
+    ImageId: 'ami-036c05bef1147a319',
     InstanceType: 't2.micro',
     KeyName: keyName,
     MaxCount: 1,
@@ -113,7 +113,7 @@ function createInstance(sgName, keyName) {
     SecurityGroups: [
       sgName,
     ],
-    UserData: 'IyEvYmluL2Jhc2gNCmN1cmwgLS1zaWxlbnQgLS1sb2NhdGlvbiBodHRwczovL3JwbS5ub2Rlc291cmNlLmNvbS9zZXR1cF8xMi54IHwgc3VkbyBiYXNoIC0NCnN1ZG8geXVtIGluc3RhbGwgLXkgbm9kZWpzDQpzdWRvIHl1bSBpbnN0YWxsIC15IGdpdA0KZ2l0IGNsb25lIGh0dHBzOi8vZ2l0aHViLmNvbS9yeWFubXVyYWthbWkvaGJmbC5naXQNCmNkIGhiZmwNCm5wbSBpDQpucG0gcnVuIHN0YXJ0',
+    UserData: 'IyEvYmluL2Jhc2gNCnN1ZG8gYXB0LWdldCB1cGRhdGUNCnN1ZG8gYXB0LWdldCAteSBpbnN0YWxsIGdpdA0KZ2l0IGNsb25lIGh0dHBzOi8vZ2l0aHViLmNvbS9yeWFubXVyYWthbWkvaGJmbC5naXQgL2hvbWUvYml0bmFtaS9oYmZsDQpjaG93biAtUiBiaXRuYW1pOiAvaG9tZS9iaXRuYW1pL2hiZmwNCmNkIC9ob21lL2JpdG5hbWkvaGJmbA0Kc3VkbyBucG0gaQ0Kc3VkbyBucG0gcnVuIHN0YXJ0',
     DryRun,
     TagSpecifications: [
       {
--- scripts/module_02/manage-ec2-instance.js
+++ scripts/module_02/manage-ec2-instance.js
@@ -1,20 +1,44 @@
 // Imports
-// TODO: Import the aws-sdk
+const AWS = require('aws-sdk');
 
-// TODO: Configure region
+const credentials = new AWS.SharedIniFileCredentials({ profile: 'edu' });
+AWS.config.update({ credentials, region: 'us-east-1' });
 
 // Declare local variables
-// TODO: Create an ec2 object
+const ec2 = new AWS.EC2();
 
 function listInstances () {
-  // TODO: List instances using ec2.describeInstances()
+  return new Promise((resolve, reject) => {
+    ec2.describeInstances({}, (err, data) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(data.Reservations.reduce((i, r) => {
+          return i.concat(r.Instances);
+        }, []));
+      }
+    });
+  });
 }
 
 function terminateInstance (instanceId) {
-  // TODO: Terminate an instance with a given instanceId
+  const params = {
+    InstanceIds: [
+      instanceId,
+    ]
+  };
+  return new Promise((resolve, reject) => {
+    ec2.terminateInstances(params, (err, data) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(data);
+      }
+    });
+  })
 }
 
-listInstances()
+// listInstances()
+// .then(data => console.log(data));
+terminateInstance('i-0de588c8174769c02')
 .then(data => console.log(data))
-// terminateInstance()
-// .then(data => console.log(data))
